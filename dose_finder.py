f2f2b01d9576f8b52c15b2fcb73c4c3d726d56df
jerem <jeremy.tellaa@tanker.io>
2021-04-06 15:55:29 +0200

    chore: use python black formatter

--- dose_finder.py
+++ dose_finder.py
@@ -10,14 +10,15 @@ from datetime import datetime
 from datetime import timedelta
 from pathlib import Path
 
+
 def search_slot(url):
     opts = Options()
     opts.headless = True
-    #assert opts.headless  # Operating in headless mode
+    # assert opts.headless  # Operating in headless mode
     browser = Firefox(options=opts)
     browser.get(url)
-    #browser.get('https://partners.doctolib.fr/hopital-public/albertville/centre-de-vaccination-covid-maison-de-sante-d-albertville?speciality_id=5494&enable_cookies_consent=1')
-    #browser.get('https://partners.doctolib.fr/centre-de-sante/faverges/centre-de-vaccination-de-faverges-seythenex?pid=practice-179054&enable_cookies_consent=1')
+    # browser.get('https://partners.doctolib.fr/hopital-public/albertville/centre-de-vaccination-covid-maison-de-sante-d-albertville?speciality_id=5494&enable_cookies_consent=1')
+    # browser.get('https://partners.doctolib.fr/centre-de-sante/faverges/centre-de-vaccination-de-faverges-seythenex?pid=practice-179054&enable_cookies_consent=1')
 
     browser.implicitly_wait(1)
 
@@ -27,7 +28,7 @@ def search_slot(url):
 
     # Sélectionner motif
     select_element = browser.find_element_by_id("booking_motive")
-    select_object = (Select(select_element))
+    select_object = Select(select_element)
 
     text_select = select_object.options[1].text
     select_object.select_by_visible_text(text_select)
@@ -50,11 +51,13 @@ def search_slot(url):
     browser.close()
     return slot
 
+
 def fetch_centres():
     url = "https://www.data.gouv.fr/fr/datasets/r/5cb21a85-b0b0-4a65-a249-806a040ec372"
     data = requests.get(url)
 
     Path("data/input/centres-vaccination.csv").write_bytes(data.content)
+
 
 def import_last_output():
     try:
@@ -63,58 +66,76 @@ def import_last_output():
         print("Last output not found. Starting from empty dict.")
         dict_json = {"last_dep_updated": "no"}
 
-    if(len(dict_json)==0):
+    if len(dict_json) == 0:
         dict_json = {"last_dep_updated": "no"}
 
     return dict_json
 
+
 def export_data(dep, slots, urls, noms, departements, departements_noms):
     dict_json = import_last_output()
 
-    dict_json[dep] = {"slots": slots, "urls": urls, "noms": noms, "scan_time": (datetime.now() + timedelta(seconds=2*3600)).strftime("%d/%m/%Y à %Hh%M")}
+    dict_json[dep] = {
+        "slots": slots,
+        "urls": urls,
+        "noms": noms,
+        "scan_time": (datetime.now() + timedelta(seconds=2 * 3600)).strftime(
+            "%d/%m/%Y à %Hh%M"
+        ),
+    }
     dict_json["last_dep_updated"] = dep
     dict_json["departements"] = departements
     dict_json["departements_noms"] = departements_noms
 
     Path("data/output/slots_dep.json").write_text(json.dumps(dict_json))
 
+
 def get_last_updated_dep():
     dict_json = import_last_output()
     print(dict_json)
     return dict_json["last_dep_updated"]
 
+
 def import_departements():
-    df = pd.read_csv(Path('data/input/departements-france.csv'))
-    return df.code_departement.astype(str).to_list(), df.nom_departement.astype(str).to_list()
+    df = pd.read_csv(Path("data/input/departements-france.csv"))
+    return (
+        df.code_departement.astype(str).to_list(),
+        df.nom_departement.astype(str).to_list(),
+    )
+
 
 def main():
     print("Starting...")
     fetch_centres()
     print("Centres fetched")
-    df = pd.read_csv(Path('data/input/centres-vaccination.csv'), sep=";", dtype={'com_cp': 'object'})
+    df = pd.read_csv(
+        Path("data/input/centres-vaccination.csv"), sep=";", dtype={"com_cp": "object"}
+    )
 
     df["com_cp"] = df["com_cp"].astype("str")
-    df = df[df.rdv_site_web.str.match(r'(.*doctolib.*)')==True]
+    df = df[df.rdv_site_web.str.match(r"(.*doctolib.*)") == True]
     departements, departements_noms = import_departements()
 
     last_updated_dep = get_last_updated_dep()
-    if(last_updated_dep=="no"):
-        id_last_updated=0
+    if last_updated_dep == "no":
+        id_last_updated = 0
     else:
-        id_last_updated = departements.index(last_updated_dep)+1
+        id_last_updated = departements.index(last_updated_dep) + 1
 
-    if(id_last_updated >= len(departements)-1):
-        id_last_updated=0
+    if id_last_updated >= len(departements) - 1:
+        id_last_updated = 0
 
-    for dep in departements[id_last_updated : min(len(departements)-1, id_last_updated+1)]:
+    for dep in departements[
+        id_last_updated : min(len(departements) - 1, id_last_updated + 1)
+    ]:
         print("DEP ======", dep)
 
-        df_dep = df[df.com_cp.str.match(r'(^{}.*)'.format(dep))==True]
+        df_dep = df[df.com_cp.str.match(r"(^{}.*)".format(dep)) == True]
         print("nb centres :", len(df_dep))
 
-        slots=[]
-        urls=[]
-        noms=[]
+        slots = []
+        urls = []
+        noms = []
         for (idx, url) in enumerate(df_dep["rdv_site_web"].values):
             print("Centre n°", idx)
             try:
@@ -127,5 +148,6 @@ def main():
                 print("not found")
 
         export_data(dep, slots, urls, noms, departements, departements_noms)
+
 
 main()
--- dose_finder_part.py
+++ dose_finder_part.py
@@ -21,6 +21,7 @@ except:
     print("Error locale")
     pass
 
+
 def search_slot(url):
     opts = Options()
     opts.headless = True
@@ -35,7 +36,7 @@ def search_slot(url):
 
     # Sélectionner motif
     select_element = browser.find_element_by_id("booking_motive")
-    select_object = (Select(select_element))
+    select_object = Select(select_element)
 
     text_select = select_object.options[1].text
     select_object.select_by_visible_text(text_select)
@@ -59,51 +60,80 @@ def search_slot(url):
 
     return slot
 
+
 def fetch_centres():
     url = "https://www.data.gouv.fr/fr/datasets/r/5cb21a85-b0b0-4a65-a249-806a040ec372"
     data = requests.get(url)
 
     Path("data/input/centres-vaccination.csv").write_bytes(data.content)
 
+
 def import_last_metadata(dep_min):
     try:
-        dict_json = json.loads(Path(f"data/output/temp/metadata{dep_min}.json").read_text())
+        dict_json = json.loads(
+            Path(f"data/output/temp/metadata{dep_min}.json").read_text()
+        )
     except:
         print("Metada not found. Starting from empty dict.")
         dict_json = {"last_dep_updated": "no"}
 
-    if(len(dict_json)==0):
+    if len(dict_json) == 0:
         dict_json = {"last_dep_updated": "no"}
 
     return dict_json
 
+
 def sort_data(slots, noms, urls):
 
-    #slots_datetime = [datetime.strptime(date[:-6] + " 2021", "%a. %d %b. %Y") for date in slots]
+    # slots_datetime = [datetime.strptime(date[:-6] + " 2021", "%a. %d %b. %Y") for date in slots]
     print(datetime.now().strftime("%a %d %b. %Y"))
-    #idx = np.argsort(slots_datetime)
+    # idx = np.argsort(slots_datetime)
 
-    #slots = list(np.array(slots)[idx])
-    #noms = list(np.array(noms)[idx])
-    #urls = list(np.array(urls)[idx])
+    # slots = list(np.array(slots)[idx])
+    # noms = list(np.array(noms)[idx])
+    # urls = list(np.array(urls)[idx])
 
     return slots, noms, urls
 
-def export_data(dep, slots, urls, noms, departements, departements_noms, dep_min, noms_pas_de_rdv, urls_pas_de_rdv, noms_autres, urls_autres):
+
+def export_data(
+    dep,
+    slots,
+    urls,
+    noms,
+    departements,
+    departements_noms,
+    dep_min,
+    noms_pas_de_rdv,
+    urls_pas_de_rdv,
+    noms_autres,
+    urls_autres,
+):
     dict_json = {}
 
     slots, noms, urls = sort_data(slots, noms, urls)
 
-    dict_json = {"slots": slots, "urls": urls, "noms": noms, "scan_time": (datetime.now() + timedelta(seconds=2*3600)).strftime("%d/%m/%Y à %Hh%M"),\
-        "urls_pas_de_rdv": urls_pas_de_rdv, "noms_pas_de_rdv": noms_pas_de_rdv,\
-        "urls_autres": urls_autres, "noms_autres": noms_autres}
+    dict_json = {
+        "slots": slots,
+        "urls": urls,
+        "noms": noms,
+        "scan_time": (datetime.now() + timedelta(seconds=2 * 3600)).strftime(
+            "%d/%m/%Y à %Hh%M"
+        ),
+        "urls_pas_de_rdv": urls_pas_de_rdv,
+        "noms_pas_de_rdv": noms_pas_de_rdv,
+        "urls_autres": urls_autres,
+        "noms_autres": noms_autres,
+    }
     dict_json["last_dep_updated"] = dep
 
     Path(f"data/output/temp/{dep}.json").write_text(json.dumps(dict_json))
 
     dict_metadata = {"last_dep_updated": dep}
 
-    Path(f"data/output/temp/metadata{dep_min}.json").write_text(json.dumps(dict_metadata))
+    Path(f"data/output/temp/metadata{dep_min}.json").write_text(
+        json.dumps(dict_metadata)
+    )
 
 
 def get_last_updated_dep(dep_min):
@@ -111,9 +141,14 @@ def get_last_updated_dep(dep_min):
 
     return dict_json["last_dep_updated"]
 
+
 def import_departements():
-    df = pd.read_csv(Path('data/input/departements-france.csv'))
-    return df.code_departement.astype(str).to_list(), df.nom_departement.astype(str).to_list()
+    df = pd.read_csv(Path("data/input/departements-france.csv"))
+    return (
+        df.code_departement.astype(str).to_list(),
+        df.nom_departement.astype(str).to_list(),
+    )
+
 
 def main():
     dep_min = int(sys.argv[1])
@@ -122,18 +157,22 @@ def main():
     print("Starting...")
     fetch_centres()
     print("Centres fetched")
-    df = pd.read_csv(Path('data/input/centres-vaccination.csv'), sep=";", dtype={'com_cp': 'object'})
+    df = pd.read_csv(
+        Path("data/input/centres-vaccination.csv"), sep=";", dtype={"com_cp": "object"}
+    )
 
     df["com_cp"] = df["com_cp"].astype("str")
     departements_all, departements_noms = import_departements()
 
-    departements = departements_all[max(0, dep_min) : min(len(departements_all)-1, dep_max)]
+    departements = departements_all[
+        max(0, dep_min) : min(len(departements_all) - 1, dep_max)
+    ]
 
     print("departements", departements)
 
     last_updated_dep = get_last_updated_dep(dep_min)
     print("last_updated_dep", last_updated_dep)
-    if(last_updated_dep=="no"):
+    if last_updated_dep == "no":
         print("if1")
         id_last_updated = dep_min
     else:
@@ -141,7 +180,7 @@ def main():
         print("index", departements_all.index(last_updated_dep))
         id_last_updated = departements_all.index(last_updated_dep) + 1
 
-    if(id_last_updated > dep_max):
+    if id_last_updated > dep_max:
         print("if2")
         id_last_updated = dep_min
 
@@ -149,14 +188,16 @@ def main():
     for dep in [departements_all[id_last_updated]]:
         print("DEP ======", dep)
 
-        df_dep_all = df[df.com_cp.str.match(r'(^{}.*)'.format(dep))==True]
-        df_dep = df_dep_all[df_dep_all.rdv_site_web.str.match(r'(.*doctolib.*)')==True]
+        df_dep_all = df[df.com_cp.str.match(r"(^{}.*)".format(dep)) == True]
+        df_dep = df_dep_all[
+            df_dep_all.rdv_site_web.str.match(r"(.*doctolib.*)") == True
+        ]
 
         print("nb centres :", len(df_dep))
 
-        slots=[]
-        urls=[]
-        noms=[]
+        slots = []
+        urls = []
+        noms = []
         noms_pas_de_rdv = []
         urls_pas_de_rdv = []
         for (idx, url) in enumerate(df_dep["rdv_site_web"].values):
@@ -174,10 +215,28 @@ def main():
                 print("not found")
 
         df_dep_liens_autre = df_dep_all[~df_dep_all.rdv_site_web.isna()]
-        df_dep_liens_autre = df_dep_liens_autre[df_dep_liens_autre.rdv_site_web.str.match(r'(.*doctolib.*)')==False]
+        df_dep_liens_autre = df_dep_liens_autre[
+            df_dep_liens_autre.rdv_site_web.str.match(r"(.*doctolib.*)") == False
+        ]
 
-        noms_autres, urls_autres = df_dep_liens_autre.nom.tolist(), df_dep_liens_autre.rdv_site_web.tolist()
+        noms_autres, urls_autres = (
+            df_dep_liens_autre.nom.tolist(),
+            df_dep_liens_autre.rdv_site_web.tolist(),
+        )
 
-        export_data(dep, slots, urls, noms, departements, departements_noms, dep_min, noms_pas_de_rdv, urls_pas_de_rdv, noms_autres, urls_autres)
+        export_data(
+            dep,
+            slots,
+            urls,
+            noms,
+            departements,
+            departements_noms,
+            dep_min,
+            noms_pas_de_rdv,
+            urls_pas_de_rdv,
+            noms_autres,
+            urls_autres,
+        )
+
 
 main()
